5f063c208053a94d795877c72897b3794e430142
Daniel <daniel.barinak@gmail.com>
2016-02-27 15:32:26 +0100

    Added files via upload

---
+++ src/mcunderground/caster/Main.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace mcunderground\caster;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+    public $configFile;
+
+    public function onEnable()
+{
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+$this->getServer()->getPluginManager()->registerEvents(new welcome($this), $this);
+        @mkdir($this->getDataFolder());
+        $this->configFile = (new Config($this->getDataFolder()."config.yml", Config::YAML, array(
+            "prefix" => "Broadcast",
+            "messageColor" => "§f",
+            "firstBColor"=> "§0",
+            "secondBColor" => "§0",
+            "prefixColor" => "§6",
+            "1Symbol" => "[",
+            "2Symbol" => "]"
+        )))->getAll();
+        $this->getLogger()->info("[Caster] Loaded!");
+    }
+
+
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args)
+{
+        switch($command->getName()){
+            case "cast":
+                if (isset($args[0])) {
+                    Server::getInstance()->broadcastMessage($this->configFile["firstBColor"].$this->configFile["1Symbol"].$this->configFile["prefixColor"].$this->configFile["prefix"].$this->configFile["secondBColor"].$this->configFile["2Symbol"]." ".$this->configFile["messageColor"].implode(" ", $args));
+                }
+                else {
+                    $sender->sendMessage("§4[Caster] §eUsage /cast (message).");
+
+                }
+                break;
+        }
+
+    }
+
+
+
+
+    public function onDisable(){
+        $this->getLogger()->info("[Caster] Disabled!");
+
+    }
+
+
+}
